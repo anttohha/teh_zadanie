1cbae90e6bc43e4c8c26f8066f23c5e371ac2bb3
anttohha <gomelnetwork@gmail.com>
2024-11-25 12:55:00 +0300

    task 2

---
+++ 2/my-nextjs-app/next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+const nextConfig = {
+  reactStrictMode: true, // Включает строгий режим React
+  swcMinify: true,       // Использует SWC для минимизации
+};
+
+module.exports = nextConfig;
